ef09fd9e215f027855c5c12c128b03bd0f7d7cb1
vvartakMoJ <vidyanand.vartak@digital.justice.gov.uk>
2024-09-17 20:36:37 +0100

    Added post test endpoint.

--- src/main/java/uk/gov/justice/laa/crime/dces/dummy/drc/controller/DummyDrcController.java
+++ src/main/java/uk/gov/justice/laa/crime/dces/dummy/drc/controller/DummyDrcController.java
@@ -1,9 +1,14 @@
 package uk.gov.justice.laa.crime.dces.dummy.drc.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpServerErrorException;
 
 @Slf4j
 @RestController
@@ -13,6 +18,14 @@ public class DummyDrcController {
   @GetMapping(value = "/test")
   public String test() {
     log.info("Get Request received to test");
-    return "GET Test Successful";
+    return "Received GET request";
+  }
+
+  @PostMapping(value = "/test")
+  public void test(@RequestBody final int requestedHttpStatus) {
+    log.info("Received POST request {}", requestedHttpStatus);
+    if (requestedHttpStatus != 500) {
+      throw new HttpServerErrorException(HttpStatusCode.valueOf(requestedHttpStatus));
+    }
   }
 }
