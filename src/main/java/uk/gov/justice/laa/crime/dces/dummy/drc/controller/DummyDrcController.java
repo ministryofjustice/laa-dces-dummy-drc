679160f5fb53ab1c07c28492bf0c53f01a8ade31
vvartakMoJ <vidyanand.vartak@digital.justice.gov.uk>
2024-09-18 15:08:43 +0100

    Added swagger and actuator endpoints.

--- src/main/java/uk/gov/justice/laa/crime/dces/dummy/drc/controller/DummyDrcController.java
+++ src/main/java/uk/gov/justice/laa/crime/dces/dummy/drc/controller/DummyDrcController.java
@@ -1,9 +1,15 @@
 package uk.gov.justice.laa.crime.dces.dummy.drc.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,11 +23,25 @@ import org.springframework.web.client.HttpServerErrorException;
 @RequestMapping("/api/v1")
 
 public class DummyDrcController {
+  @Operation(description = "Test GET endpoint to use with DCES integration.")
   @GetMapping(value = "/test")
   public String test() {
     log.info("Get Request received to test");
     return "Received GET request";
   }
+
+  @Operation(description = "Test POST endpoint to use with DCES integration.")
+  @ApiResponse(responseCode = "200",
+      content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+          schema = @Schema(implementation = String.class)))
+  @ApiResponse(responseCode = "400",
+      description = "Bad request.",
+      content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+          schema = @Schema(implementation = ErrorResponse.class)))
+  @ApiResponse(responseCode = "500",
+      description = "Server Error.",
+      content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+          schema = @Schema(implementation = ErrorResponse.class)))
 
   @PostMapping(value = "/test")
   public ResponseEntity<String> test(@RequestBody final int requestedHttpStatus) {
